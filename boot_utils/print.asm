8772a70474f5fd544d326ba9f3fbd8deca6a0e22
Curtis <132358716+RedFoxMain@users.noreply.github.com>
2025-07-31 14:23:56 +0300

    Update print.asm

--- boot_utils/print.asm
+++ boot_utils/print.asm
@@ -6,11 +6,10 @@ global print_string
 print_string:
 	mov ah, 0x0E
 print_string_while_not_null:
-	mov al, [si]
+	lodsb
 	cmp al, 0
 	je print_string_exit
 	int 0x10
-	inc si
 	jmp print_string_while_not_null
 print_string_exit:
 	ret
